70fa4119794399a8956053412f2f02e9245106a9
Nathan <nathasy234@gmail.com>
2023-11-08 20:28:30 +0700

    Quiz Project 2.2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import requests
 from datetime import datetime
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -11,7 +12,19 @@ db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
-    return render_template("index.html")
+    words_result = db.words.find({}, {'_id': False})
+    words = []
+    for word in words_result:
+        definition = word['definitions'][0]['shortdef']
+        definition = definition if type(definition) is str else definition[0]
+        words.append(
+            {
+            'word': word['word'],
+            'definition': definition,
+            }
+        )
+    msg = request.args.get('msg')
+    return render_template("index.html", words=words, msg=msg)
 
 
 @app.route('/detail/<keyword>')
@@ -20,6 +33,20 @@ def detail(keyword):
     url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{keyword}?key={api_key}'
     response = requests.get(url)
     definitions = response.json()
+
+    if not definitions :
+        return redirect(url_for(
+            'main',
+            msg=f'Could not find the word, "{keyword}"'
+        ))
+    
+    if type(definitions[0]) is str:
+         suggestions = ', '.join(definitions)
+         return redirect(url_for(
+            'main',
+            msg=f'Could not find the word, "{keyword}", did you mean one of these words: {suggestions}'
+        ))
+
     status = request.args.get('status_give', 'new')
     return render_template(
         'detail.html',
@@ -49,9 +76,60 @@ def save_word():
 def delete_word():
     word = request.form.get('word_give')
     db.words.delete_one({'word': word})
+    db.examples.delete_many({'word': word})
     return jsonify({
         'result': 'success',
         'msg': f'the word {word} was deleted'
+    })
+
+
+@app.route('/msg')
+def msg():
+    word = request.args.get('word')
+    suggestions = request.args.get('suggestions')
+    if suggestions:
+        suggestions = suggestions.split(',')
+    return render_template('msg.html', word=word, suggestions=suggestions)
+
+
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs():
+    word = request.args.get('word')
+    example_data = db.examples.find({'word': word})
+    examples = []
+    for example in example_data:
+        examples.append({
+            'example': example.get('example'),
+            'id': str(example.get('_id')),
+        })
+    return jsonify({
+        'result': 'success', 
+        'examples': examples
+    })
+
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    word = request.form.get('word')
+    example = request.form.get('example')
+    doc = {
+        'word': word,
+        'example': example,
+    }
+    db.examples.insert_one(doc)
+    return jsonify({
+        'result': 'success',
+        'msg': f'Your example, {example}, for the word, {word}, was saved!!',
+    })
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    id = request.form.get('id')
+    word = request.form.get('word')
+    db.examples.delete_one({'_id': ObjectId(id)})
+    return jsonify({
+        'result': 'success',
+        'msg': f'Your example for the word, {word}, was deleted!!',
     })
 
 
